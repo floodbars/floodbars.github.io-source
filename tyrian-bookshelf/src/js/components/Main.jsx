c822ba4fca8e54c18cfe795a2eeace1e47651d31
floodbars <45854762+floodbars@users.noreply.github.com>
2019-04-19 22:20:03 -0700

    [Tyrian Bookshelf] Move network calls out of presentation components
    - Slightly improve error handling

--- tyrian-bookshelf/src/js/components/Main.jsx
+++ tyrian-bookshelf/src/js/components/Main.jsx
@@ -12,13 +12,20 @@ class Main extends React.Component {
       books: null,
       areAchievementsLoaded: false,
       achievements: null,
-      filters: {}
+      repository: null,
+      filters: {},
+      areAccountAchievementsLoaded: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
+    this.loadAchievements();
+    this.loadBooks();
+  }
+
+  loadAchievements() {
     fetch("https://floodbars.github.io/gw2/data/bookshelf-achievements.json")
       .then(res => res.json())
       .then(
@@ -41,6 +48,9 @@ class Main extends React.Component {
           });
         }
       )
+  }
+
+  loadBooks() {
     fetch("https://floodbars.github.io/gw2/data/bookshelf-books.json")
       .then(res => res.json())
       .then(
@@ -65,39 +75,110 @@ class Main extends React.Component {
       )
   }
 
-  render() {
-    const { error, areBooksLoaded, areAchievementsLoaded, books, achievements, preview } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!areBooksLoaded || !areAchievementsLoaded) {
-      return <div>Loading data...</div>;
-    } else {
-      return (
-        <div className="row">
-          <div className="col p-0">
-            <nav class="navbar navbar-dark bg-primary sticky-top shadow mb-2">
-              <a class="navbar-brand" href="#">Tyrian Bookshelf</a>
-            </nav>
-            <form onSubmit={this.handleSubmit} className="p-2">
-              <div className="form-group">
-              <label htmlFor="api-key">
-                API Key
-              </label>
-              <input
-                id="api-key"
-                className="form-control"
-                onChange={this.handleChange}
-                value={this.state.apiKey}
-              />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Load
-              </button>
-            </form>
-          </div>
-        </div>
-      );
+  loadAccountAchievements() {
+    const repository = new BookRepository(this.state.books, this.state.achievements);
+    this.setState({
+      repository: repository
+    });
+
+    if (this.state.apiKey == 'UNLOCK_ALL') {
+      const allUnlocked = repository.generateCompleteProgress();
+      this.setState({
+        achievements: allUnlocked,
+        areAccountAchievementsLoaded: true
+      });
+      return;
+    } else if (this.state.apiKey == 'UNLOCK_NONE') {
+      this.setState({
+        achievements: [],
+        areAccountAchievementsLoaded: true
+      });
+      return;
+    } else if (this.state.apiKey == 'UNLOCK_SOME') {
+      const someUnlocked = repository.generatePartialProgress();
+      this.setState({
+        achievements: someUnlocked,
+        areAccountAchievementsLoaded: true
+      });
+      return;
     }
+    // TODO wrap this in an API-centric class/module
+    var url = "https://api.guildwars2.com/v2/account/achievements?"
+      + "access_token=" + this.state.apiKey;
+    fetch(url)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          if (res.status == 403) {
+            throw new Error("Please supply a valid API key with 'progression' scope")
+          } else {
+            // TODO not sure how to properly expose promises.
+            throw new Error(res.text());
+          }
+        }
+      })
+      .then(
+        (result) => {
+          this.setState({
+            achievements: result,
+            areAccountAchievementsLoaded: true
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, areBooksLoaded, areAchievementsLoaded, books, achievements, repository, preview, areAccountAchievementsLoaded } = this.state;
+    if (areAccountAchievementsLoaded) {
+      ReactDOM.render(
+        <Shelf achievements={achievements} repository={repository} />,
+        document.getElementById('root')
+      );
+      return null;
+    }
+
+    var interjection = null;
+    if (error) {
+      interjection = <div className="alert alert-danger m-4"><b>Error</b>: {error.message}</div>;
+    } else if (!areBooksLoaded || !areAchievementsLoaded) {
+      interjection = <div className="alert alert-info m-4">Loading data...</div>;
+    }
+
+    return (
+      <div className="row">
+        <div className="col p-0">
+          <nav class="navbar navbar-dark bg-primary sticky-top shadow mb-2">
+            <a class="navbar-brand" href="#">Tyrian Bookshelf</a>
+          </nav>
+          {interjection}
+          <form onSubmit={this.handleSubmit} className="p-2">
+            <div className="form-group">
+            <label htmlFor="api-key">
+              API Key
+            </label>
+            <input
+              id="api-key"
+              className="form-control"
+              onChange={this.handleChange}
+              value={this.state.apiKey}
+            />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Load
+            </button>
+          </form>
+        </div>
+      </div>
+    );
   }
 
   handleChange(e) {
@@ -109,14 +190,8 @@ class Main extends React.Component {
     if (!this.state.apiKey.length) {
       return;
     }
-    this.setState(state => ({
-      apiKey: state.apiKey
-    }));
-    const repository = new BookRepository(this.state.books, this.state.achievements);
-    ReactDOM.render(
-      <Shelf apiKey={this.state.apiKey} repository={repository} />,
-      document.getElementById('root')
-    );
+
+    this.loadAccountAchievements();
   }
 }
 
--- tyrian-bookshelf/src/js/components/presentational/Shelf.jsx
+++ tyrian-bookshelf/src/js/components/presentational/Shelf.jsx
@@ -9,9 +9,6 @@ class Shelf extends React.Component {
     super(props);
 
     this.state = {
-      achievements: [],
-      isLoaded: false,
-      error: null,
       selection: null,
       filters: {
         complete: true,
@@ -21,52 +18,7 @@ class Shelf extends React.Component {
       showConfig: false
     };
   }
-  componentDidMount() {
-    if (this.props.apiKey == 'UNLOCK_ALL') {
-      const allUnlocked = this.props.repository.generateCompleteProgress();
-      this.setState({
-        achievements: allUnlocked,
-        isLoaded: true
-      });
-      return;
-    } else if (this.props.apiKey == 'UNLOCK_NONE') {
-      this.setState({
-        achievements: [],
-        isLoaded: true
-      });
-      return;
-    } else if (this.props.apiKey == 'UNLOCK_SOME') {
-      const someUnlocked = this.props.repository.generatePartialProgress();
-      this.setState({
-        achievements: someUnlocked,
-        isLoaded: true
-      });
-      return;
-    }
-    // TODO wrap this in an API-centric class/module
-    var url = "https://api.guildwars2.com/v2/account/achievements?"
-      + "access_token=" + this.props.apiKey;
-    fetch(url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            achievements: result,
-            isLoaded: true
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-  
+
   previewBook(book) {
     this.setState({
       selection: book
@@ -89,48 +41,44 @@ class Shelf extends React.Component {
   }
 
   render() {
-    const { error, isLoaded, achievements, selection, filters, showConfig } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading achievement progress...</div>;
-    } else {
-      var books = this.props.repository.mapAchievementsToBooks(achievements);
-      books.sort((x, y) => x["name"].localeCompare(y["name"]));
-      return (
-        <div className="codex row h-100">
-          <nav className="col-sm-4 h-100 p-0" style={{overflowY: "auto"}}>
-            <nav class="navbar navbar-dark bg-primary sticky-top shadow mb-2">
-              <a class="navbar-brand" href="#">Tyrian Bookshelf</a>
-              <button
-                  className="btn btn-primary"
-                  type="button"
-                  onClick={() => this._toggleConfiguration()}>
-                &#9881;
-              </button>
-            </nav>
-            <div className={"card bg-light border-primary m-4 p-3 " + (showConfig ? '' : 'collapse')} id="bookshelfConfig">
-              <Filter
-                filters={filters}
-                onSelectionChange={this._onSelectionChange.bind(this)} />
-            </div>
-            <div className="list-group p-2">
-              {books.map(book => (
-                <Book
-                  key={book.name}
-                  activeBook={selection == null ? null : selection.name}
-                  value={book}
-                  onClick={() => this.previewBook(book)}
-                  filters={filters} />
-              ))}
-            </div>
+    const { error, isLoaded, selection, filters, showConfig } = this.state;
+
+    var books = this.props.repository.mapAchievementsToBooks(this.props.achievements);
+    books.sort((x, y) => x["name"].localeCompare(y["name"]));
+
+    return (
+      <div className="codex row h-100">
+        <nav className="col-sm-4 h-100 p-0" style={{overflowY: "auto"}}>
+          <nav class="navbar navbar-dark bg-primary sticky-top shadow mb-2">
+            <a class="navbar-brand" href="#">Tyrian Bookshelf</a>
+            <button
+                className="btn btn-primary"
+                type="button"
+                onClick={() => this._toggleConfiguration()}>
+              &#9881;
+            </button>
           </nav>
-          <main className="col-sm-8 h-100 p-3" style={{overflowY: "auto"}}>
-            <Preview book={selection} />
-          </main>
-        </div>
-      );
-    }
+          <div className={"card bg-light border-primary m-4 p-3 " + (showConfig ? '' : 'collapse')} id="bookshelfConfig">
+            <Filter
+              filters={filters}
+              onSelectionChange={this._onSelectionChange.bind(this)} />
+          </div>
+          <div className="list-group p-2">
+            {books.map(book => (
+              <Book
+                key={book.name}
+                activeBook={selection == null ? null : selection.name}
+                value={book}
+                onClick={() => this.previewBook(book)}
+                filters={filters} />
+            ))}
+          </div>
+        </nav>
+        <main className="col-sm-8 h-100 p-3" style={{overflowY: "auto"}}>
+          <Preview book={selection} />
+        </main>
+      </div>
+    );
   }
 }
 
